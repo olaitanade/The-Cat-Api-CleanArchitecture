456bcf3b9754ab3125f651f9da35be2f48784db1
Adetayo Olaitan <olaitan.adetayo.com>
2021-03-21 18:29:17 +0100

    added the api key

--- app/src/main/java/com/vastinc/thecatexperience/data/remote/api/CatApi.java
+++ app/src/main/java/com/vastinc/thecatexperience/data/remote/api/CatApi.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 
 public interface CatApi {
+    @Headers({"x-api-key: a2b97797-8cc6-40f6-8103-a023d7ca623c"})
     @GET(Production.BREEDS)
     Call<List<Breed>> getBreeds();
 }
